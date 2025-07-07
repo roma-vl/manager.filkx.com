3ead4e51e058f001acf005566cbbcb567eec09a1
Roma Volkov <Drakyla60@gmail.com>
2025-07-08 00:40:55 +0300

    add crud

---
+++ manager/src/Controller/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('lucky/number.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
