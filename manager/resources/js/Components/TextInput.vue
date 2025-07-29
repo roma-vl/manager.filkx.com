114e4c108a033791dafe765e5447c3b7e2eabaf4
Roma Volkov <Drakyla60@gmail.com>
2025-07-30 01:47:18 +0300

    fix components

--- manager/resources/js/Components/TextInput.vue
+++ manager/resources/js/Components/TextInput.vue
@@ -1,26 +1,26 @@
 <script setup>
-  import { onMounted, ref } from 'vue'
+import { onMounted, ref } from 'vue'
 
-  const model = defineModel({
+const model = defineModel({
     type: String,
     required: true,
-  })
+})
 
-  const input = ref(null)
+const input = ref(null)
 
-  onMounted(() => {
+onMounted(() => {
     if (input.value.hasAttribute('autofocus')) {
-      input.value.focus()
+        input.value.focus()
     }
-  })
+})
 
-  defineExpose({ focus: () => input.value.focus() })
+defineExpose({ focus: () => input.value.focus() })
 </script>
 
 <template>
-  <input
-    ref="input"
-    v-model="model"
-    class="rounded-md border-gray-300 shadow-md focus:border-indigo-500 focus:ring-indigo-500 p-2"
-  />
+    <input
+        ref="input"
+        v-model="model"
+        class="w-full p-2 rounded-md border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 text-gray-900 dark:text-gray-100 shadow-md focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 transition placeholder-gray-400 dark:placeholder-gray-500"
+    />
 </template>
--- manager/resources/js/Layouts/AppLayout.vue
+++ manager/resources/js/Layouts/AppLayout.vue
@@ -223,7 +223,9 @@ onBeforeUnmount(() => {
             {{ flash.success }}
           </div>
           <div
-            class="mx-auto p-3 rounded-lg bg-white text-gray-800 shadow-md shadow-gray-200/50 dark:bg-gradient-to-br dark:from-indigo-900 dark:via-gray-900 dark:to-[#0e0f11] dark:text-indigo-200 dark:shadow-indigo-900/40 transition-all duration-300 ease-in-out"
+            class="mx-auto p-3 rounded-lg bg-white text-gray-800 shadow-md shadow-gray-200/50 dark:bg-gradient-to-br
+             dark:from-indigo-900 dark:via-gray-900 dark:to-[#0e0f11] dark:text-indigo-200
+             dark:shadow-indigo-900/40 transition-all duration-300 ease-in-out min-h-[400px]"
             role="main"
           >
             <slot />
