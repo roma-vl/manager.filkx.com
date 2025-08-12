f6b6dd9dc2ecfcb2d280cbb69c5e4b7cbe6fcee0
Roma Volkov <Drakyla60@gmail.com>
2025-08-12 20:25:28 +0300

    asign task account

--- manager/src/Model/Work/UseCase/Projects/Task/Executor/Assign/Command.php
+++ manager/src/Model/Work/UseCase/Projects/Task/Executor/Assign/Command.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Model\Work\UseCase\Projects\Task\Executor\Assign;
 
+use App\Model\User\Entity\Account\Account;
 use Symfony\Component\Validator\Constraints as Assert;
 
 class Command
@@ -15,9 +16,13 @@ class Command
     #[Assert\NotBlank]
     public array $members;
 
-    public function __construct(string $actor, int $id)
+    #[Assert\NotBlank]
+    public Account $account;
+
+    public function __construct(string $actor, int $id, Account $account)
     {
         $this->actor = $actor;
         $this->id = $id;
+        $this->account = $account;
     }
 }
